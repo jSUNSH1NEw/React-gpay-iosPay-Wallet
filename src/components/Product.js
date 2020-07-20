d4c2b20001d6d00cf59facde5f84b878e6e48ef8
sunshine <weertz.joffrey@protonmail.ch>
2020-07-20 10:25:35 +0200

    updateFunction# Function Working with data

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import products from '../data/products';
+import products from './data/products';
 import { useShoppingCart, formatCurrencyString } from 'use-shopping-cart';
 
 const Products = () => {
